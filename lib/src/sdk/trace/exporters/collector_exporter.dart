64d926ddaa44177918d3103503ff0163d583d3af
7ssam <hussamhajjar.it@gmail.com>
2022-10-17 13:53:55 +0300

    Revert "chore: use writeToJson instead of bytes"
    This reverts commit 3c7a90ffd4365460cc82674f80fd4d215d099c8b.

--- lib/src/sdk/trace/exporters/collector_exporter.dart
+++ lib/src/sdk/trace/exporters/collector_exporter.dart
@@ -36,7 +36,7 @@ class CollectorExporter implements api.SpanExporter {
         resourceSpans: _spansToProtobuf(spans));
 
     client.post(uri,
-        body: body.writeToJson(),
+        body: body.writeToJsonMap(),
         headers: {'Content-Type': 'application/x-protobuf'});
   }
 
